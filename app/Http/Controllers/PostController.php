860b591ee326e7133bdb6d2c9dfacca57f7e4d97
Rokaya-mk <mbairikrqia@gmail.com>
2022-01-29 11:07:06 +0100

    add authentifiation package to project

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,6 +10,13 @@ use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
+
+    //construct
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
     public function index()
     {
 
